d1be15b4719ef4002d5191324b8ddea94a74626a
Rokstar890 <144723163+Rokstar890@users.noreply.github.com>
2023-10-12 11:35:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
+API_ID       = 23762978
+API_HASH     = "eff30dac5504a8660e69bfe19f668571"
+BOT_TOKEN    = "6539307245:AAH_yBu2LZosKVumzDZ657gafNxU9PLCRTI"
 SESSION      = ""
-DATABASE_URI = ""
+DATABASE_URI = "mongodb+srv://hebapir376:ravi1234@cluster0.x9pmpfx.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  =
-ADMIN        = 1255023013
-CHANNEL      = "BotszList"
+ADMIN        = 1216307744
+CHANNEL      = "NETFLIXVILLA_INDIA"
